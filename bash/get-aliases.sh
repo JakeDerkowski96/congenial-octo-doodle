4f96b25806f09f14e8a986adfbea3ad37be801dc
Jake Derkowski <jakederkowski12@gmail.com>
2021-03-15 16:48:43 -0500

    bash1.1

--- bash/get-aliases.sh
+++ bash/get-aliases.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # download my common bash aliases
 
-wget 
+wget https://raw.githubusercontent.com/JakeDerkowski96/congenial-octo-doodle/uno/bash/aliases
+cp aliases ~/.bash_alises
+source .bashrc
+
+# touch ~/.bash_alises
